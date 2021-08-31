1473169cf397fa4d0f45bd5402b99c40ae190bcf
amiller0823 <amiller@air.org>
2021-08-31 08:59:32 -0500

    Dynamic filtering of lines based on blank space and colon

--- pdf_txt_2_df.py
+++ pdf_txt_2_df.py
@@ -25,30 +25,30 @@
 
 import pandas as pd
 
-
 df = pd.DataFrame(columns=['line'], dtype='string')
 
 txt_file_path = "./text_versions/HEERF Quarterly Budget and Expenditure Reporting - V1_3_1.txt"
 
 def extractLines(txtfile):    
-    lines_to_read = range(163,232)
+    #lines_to_read = range(163,232)
     fp = open(txtfile, 'r', encoding='UTF-8')
     lines = fp.readlines()
-
     for item in lines:
-        idx = lines.index(item)
-        df_idx = idx - 163
-        if idx in lines_to_read:
-            df.loc[df_idx,'line']=item
-            df.loc[df_idx,'trim']=item.strip()
+        if ((not (u"\u00A0" in item)) & (":" in item)):
+            idx = lines.index(item)
+            #df_idx = idx - 163
+            df.loc[idx,'line']=item
+            df.loc[idx,'line2']=item.strip()
+    #df.reset_index()
     fp.close()
 
-for item in df['trim']:
-    idx = list(df['trim']).index(item)
+extractLines(txt_file_path)
+df.index = pd.RangeIndex(len(df.index))
+
+for item in df['line2']:
+    idx = list(df['line2']).index(item)
     splitList = item.split(':')
     question = splitList[0].strip()
     ans = splitList[1].strip()
-   # df['split'] = splitList
     df.loc[idx,'question'] = question
     df.loc[idx,'answer'] = ans
-        
