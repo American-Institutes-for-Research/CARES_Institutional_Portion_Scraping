806c2aace21cd9ec168af6c3bfee52c129852449
amiller0823 <amiller@air.org>
2021-09-13 13:14:06 -0500

    iterate through text_version folders

--- pdf_txt_2_df.py
+++ pdf_txt_2_df.py
@@ -61,7 +61,7 @@ def extractLines2(txtfile):
     #lines_to_read = range(163,232)
     fp = open(txtfile, 'r', encoding='UTF-8')
     lines = fp.readlines()
-    idx_list = []
+#    idx_list = []
     for item in lines:
         if ("Institution Name:" in item):
             start_idx = lines.index(item)
@@ -89,5 +89,58 @@ for item in df['line2']:
     df.loc[idx,'answer'] = ans
 
 #%%
+import os
+import glob
+import pandas as pd
 
-root = "/text_versions/text_versions/"
+df = pd.DataFrame(columns=['line'], dtype='string')
+
+# for filename in os.listdir(os.getcwd()):
+#     with open(os.path.join(os.getcwd(), filename), 'r') as f: #open in readonly mode
+#         print(filename)
+
+root_dir = '.'
+text_files_dir = os.path.join(root_dir, 'text_versions/text_versions')
+#%%
+# def get_start_idx(file_path):
+#     fp = open(file_path, 'r', encoding='UTF-8')
+#     lines = fp.readlines()
+#     for item in lines:
+#         if ("Institution Name:" in item):
+#             idx = lines.index(item)
+#             return idx
+
+#%%
+def extractLines(txtfile):    
+    #lines_to_read = range(163,232)
+    fp = open(txtfile, 'r', encoding='UTF-8')
+    lines = fp.readlines()
+    idx_list = []
+    # for item in lines:
+    #     if ("Institution Name:" in item):
+    #         global start_idx
+    #         start_idx = lines.index(item)
+    for item in lines:
+        if ((not (u"\u00A0" in item)) & (":" in item) & (not("http" in item))):
+            idx = lines.index(item)
+            df.loc[idx,'line']=item
+            df.loc[idx,'line2']=item.strip()
+    #df.reset_index()
+    fp.close()
+
+#%%
+for folder in os.listdir(text_files_dir):
+    #print(folder)
+    if folder.startswith('.'):
+        print("Skipping folder {}".format(folder))
+        continue
+    for file in os.listdir(os.path.join(text_files_dir,folder)):
+        filename = "{}".format(file)
+        file_path = os.path.join(os.path.join(text_files_dir,folder),filename)
+        extractLines(file_path)
+        df.index = pd.RangeIndex(len(df.index))
+        for item in df['line2']:
+            idx = list(df['line2']).index(item)
+            df.loc[idx,'school'] = folder
+            df.loc[idx,'file'] = file
+            splitList = item.split(':')
